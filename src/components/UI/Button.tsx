025956d66d4fe9b3a5c4f1d809bb4255c7c387e3
amitytobi <amityekoyi@gmail.com>
2026-07-03 23:20:34 +0100

    refactor: type Button component props

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,14 +1,16 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: string;
+}
+
 export default function Button({
   children,
   variant = "primary",
   className = "",
   ...props
-}) {
+}: ButtonProps) {
   return (
-    <>
-      <button className={`btn btn-${variant} ${className}`} {...props}>
-        {children}
-      </button>
-    </>
+    <button className={`btn btn-${variant} ${className}`} {...props}>
+      {children}
+    </button>
   );
 }
