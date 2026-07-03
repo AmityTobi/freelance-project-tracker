5387d653ffc2fef04096f880df64f5828cf532ac
amitytobi <amityekoyi@gmail.com>
2026-07-03 21:40:20 +0100

    refactor: type TaskList and TaskItem components

--- src/components/Tasks/TaskItem.tsx
+++ src/components/Tasks/TaskItem.tsx
@@ -3,14 +3,21 @@ import { useRef } from "react";
 import Modal from "../UI/Modal";
 import Button from "../UI/Button";
 import { useAppContext } from "../../store/AppContext";
+import { ModalHandle } from "../UI/Modal";
+import { Task } from "../../types/client";
 
-export default function TaskItem({ task, projectId }) {
+interface TaskItemProps {
+  projectId: string;
+  task: Task;
+}
+
+export default function TaskItem({ task, projectId }: TaskItemProps) {
   const { onToggleTask, onDeleteTask } = useAppContext();
 
-  const modalRef = useRef();
+  const modalRef = useRef<ModalHandle>(null);
 
   function showModal() {
-    modalRef.current.show();
+    modalRef.current?.show();
   }
 
   return (
@@ -22,19 +29,19 @@ export default function TaskItem({ task, projectId }) {
         ref={modalRef}
         onConfirm={() => {
           onDeleteTask(projectId, task.id);
-          modalRef.current.close();
+          modalRef.current?.close();
         }}
-        onCancel={() => modalRef.current.close()}
+        onCancel={() => modalRef.current?.close()}
         message="Are you sure you want to delete this task?"
       />
 
       <span className="task-check">{task.completed ? "✓" : "○"}</span>
-      <span className="task-desc">{task.desc}</span>
+      <span className="task-desc">{task.title}</span>
 
       <Button
         variant="danger"
         className="btn-xs"
-        onClick={(e) => {
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
           e.stopPropagation();
           showModal();
         }}
