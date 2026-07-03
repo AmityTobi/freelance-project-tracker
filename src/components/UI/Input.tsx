45755a07c4eb46b9a3f8bc37d3fc02efc850273a
amitytobi <amityekoyi@gmail.com>
2026-07-03 23:29:37 +0100

    refactor: type Input component props

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,4 +1,10 @@
-export default function Input({ label, id, error, ...props }) {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  id: string;
+  error: string | null;
+}
+
+export default function Input({ label, id, error, ...props }: InputProps) {
   return (
     <>
       <label htmlFor={id}>{label}</label>
